ab1cc12de79cc34ab7304312af70c7286786fcaa
rotoosoft <lyzkhqby@sina.com>
2016-06-08 17:38:54 +0800

    new

---
+++ src/IntersectionOfTwoArraysII.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by rotoosoft-d04 on 2016/6/8.
+ */
+public class IntersectionOfTwoArraysII {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        List<Integer> res = new ArrayList<>();
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int i = 0, j = 0;
+        int index = 0;
+        while(i < len1 && j < len2) {
+            if(nums1[i] == nums2[j]) {
+                res.add(nums1[i]);
+                index++;
+                i++;
+                j++;
+            }else if(nums1[i] < nums2[j]) {
+                i++;
+            }else {
+                j++;
+            }
+        }
+
+        int len = res.size();
+        int[] result = new int[len];
+        for (int k = 0; k < len; k++) {
+            result[k] = res.get(k);
+        }
+        return result;
+
+    }
+}
