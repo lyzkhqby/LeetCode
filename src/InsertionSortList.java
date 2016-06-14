b692cd919e466a1b0ed9e42478e791926fad8154
rotoosoft <lyzkhqby@sina.com>
2016-06-14 15:17:27 +0800

    new

---
+++ src/InsertionSortList.java
@@ -0,0 +1,27 @@
+/**
+ * Created by rotoosoft-d04 on 2016/6/14.
+ */
+public class InsertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(0);
+
+        while(head != null) {
+            ListNode node = dummy;
+            while(node.next != null && node.next.val < head.val) {
+                node = node.next;
+            }
+            ListNode temp = head.next;
+            head.next = node.next;
+            node.next = head;
+            head = temp;
+        }
+
+        return dummy.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
