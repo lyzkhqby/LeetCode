98402e397811075c2f6b20b8b3ec3092b2f59eb3
lyzkhqby <lyzkhqby@sina.com>
2016-06-26 16:07:25 +0800

    new

---
+++ src/ValidParentheses.java
@@ -0,0 +1,28 @@
+/**
+ * Created by zk on 2016/6/26.
+ */
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        char[] stack = new char[s.length()];
+        int head = 0;
+        for (char c : s.toCharArray()) {
+            switch(c) {
+                case '(':
+                case '{':
+                case '[':
+                    stack[head++] = c;
+                    break;
+                case ']':
+                    if(head == 0 || stack[--head] != '[') return false;
+                    break;
+                case '}':
+                    if(head == 0 || stack[--head] != '{') return false;
+                    break;
+                case ')':
+                    if(head == 0 || stack[--head] != '(') return false;
+                    break;
+            }
+        }
+        return head == 0;
+    }
+}
