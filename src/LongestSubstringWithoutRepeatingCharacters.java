afc72d2011d2797899bc8ebd79f90815524fd394
rotoosoft <lyzkhqby@sina.com>
2016-07-07 16:36:34 +0800

    new

---
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+/**
+ * Created by rotoosoft-d04 on 2016/7/7.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        int[] map = new int[256];
+        Arrays.fill(map, -1);
+        int fast = 0, slow = 0, ans = 0, len = s.length();
+        for (; fast < len; fast++) {
+            int ch = s.charAt(fast);
+            while(map[ch] != -1 && slow < fast) {
+                int temp = s.charAt(slow);
+                map[temp] = -1;
+                slow++;
+            }
+            map[ch] = 0;
+            ans = Math.max(ans, fast - slow + 1);
+        }
+        return ans;
+    }
+}
