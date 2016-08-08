09f4e8220507ae1b29321afa2e403b267aa94c81
rotoosoft <lyzkhqby@sina.com>
2016-08-08 13:13:47 +0800

    new

---
+++ src/GuessNumberHigherOrLower.java
@@ -0,0 +1,25 @@
+/**
+ * Created by rotoosoft-d04 on 2016/8/8.
+ */
+public class GuessNumberHigherOrLower {
+    public int guessNumber(int n) {
+        int low = 0;
+        int high = n;
+        int mid = low + ((high-low)>>1);
+        int answer = guess(mid);
+        while (answer != 0) {
+            if(answer == -1) {
+                high = mid - 1;
+            }else if(answer == 1) {
+                low = mid + 1;
+            }
+            mid = low + ((high-low)>>1);
+            answer = guess(mid);
+        }
+        return mid;
+    }
+
+    private int guess(int num){
+        return 0;//内置api
+    }
+}
