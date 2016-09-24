bbc0a5f154c3317ade40404b197b988383621629
lyzkhqby <lyzkhqby@sina.com>
2016-09-24 16:16:49 +0800

    new

---
+++ src/InsertDeleteGetRandom.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+/**
+ * Created by zk on 2016/9/24.
+ */
+public class InsertDeleteGetRandom {
+    ArrayList<Integer> nums;
+    HashMap<Integer, Integer> locs;
+
+    Random rand = new Random();
+
+    /** Initialize your data structure here. */
+    public InsertDeleteGetRandom() {
+        nums = new ArrayList<>();
+        locs = new HashMap<>();
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if (locs.containsKey(val)) return false;
+        locs.put(val, nums.size());
+        nums.add(val);
+        return true;
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if (!locs.containsKey(val)) return false;
+        int loc = locs.get(val);
+        if (loc < nums.size() - 1) {
+            int lastOne = nums.get(nums.size() - 1);
+            nums.set(loc, lastOne);
+            locs.put(lastOne, loc);
+        }
+        locs.remove(val);
+        nums.remove(nums.size() - 1);
+        return true;
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return nums.get(rand.nextInt(nums.size()));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
