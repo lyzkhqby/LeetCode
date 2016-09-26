2cf0fdb762c08d6bdefb4a0e73cbae5c97ab18c1
rotoosoft <lyzkhqby@sina.com>
2016-09-26 10:52:04 +0800

    new

---
+++ src/FactorialTrailingZeroes.java
@@ -0,0 +1,13 @@
+/**
+ * Created by rotoosoft-d04 on 2016/9/26.
+ */
+public class FactorialTrailingZeroes {
+    public int trailingZeroes(int n) {
+        int res = 0;
+        while (n >= 5) {
+            res += n / 5;
+            n /= 5;
+        }
+        return res;
+    }
+}
