5b0779a6ece09029ee56b5abdb20377fc05e1e70
rotoosoft <lyzkhqby@sina.com>
2016-09-30 11:04:01 +0800

    new

---
+++ src/UglyNumber.java
@@ -0,0 +1,14 @@
+/**
+ * Created by rotoosoft-d04 on 2016/9/30.
+ */
+public class UglyNumber {
+    public boolean isUgly(int num) {
+        if (num == 0) return false;
+        if (num == 1) return true;
+        while (num >= 2 && num % 2 == 0) num /= 2;
+        while (num >= 3 && num % 3 == 0) num /= 3;
+        while (num >= 5 && num % 5 == 0) num /= 5;
+
+        return num == 1;
+    }
+}
