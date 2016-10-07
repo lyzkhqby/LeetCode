38cfeb1d875c1391e5a35ccfe95c5af3d7dfa182
lyzkhqby <lyzkhqby@sina.com>
2016-10-07 20:34:46 +0800

    new

---
+++ src/PerfectSquares.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+/**
+ * Created by zk on 2016/10/7.
+ */
+public class PerfectSquares {
+    public int numSquares(int n) {
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, Integer.MAX_VALUE);
+        memo[0] = 0;
+        int i = 0;
+        while (++i * i <= n) {
+            for (int j = i * i; j <= n; j++) {
+                memo[j] = Math.min(memo[j], memo[j - i * i] + 1);
+            }
+        }
+        return memo[n];
+    }
+}
