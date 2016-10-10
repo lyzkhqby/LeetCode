dfd1fef1af04a2a7d54df3290b8594153fd09eaa
rotoosoft <lyzkhqby@sina.com>
2016-10-10 15:56:41 +0800

    new

---
+++ src/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,25 @@
+/**
+ * Created by rotoosoft-d04 on 2016/10/10.
+ */
+public class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode node = head;
+        while (node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            }else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
