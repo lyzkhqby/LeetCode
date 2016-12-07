17356a70026d401ff4090d24042cafd80be458e5
rotoosoft <lyzkhqby@sina.com>
2016-12-07 18:05:08 +0800

    new

---
+++ src/UniqueSubstringsInWraparoundString.java
@@ -0,0 +1,25 @@
+/**
+ * Created by rotoosoft-d04 on 2016/12/7.
+ */
+public class UniqueSubstringsInWraparoundString {
+    public int findSubstringInWraproundString(String p) {
+        int[] count = new int[26];
+        int maxLengthCur = 0;
+        for (int i = 0; i < p.length(); i++) {
+            if (i > 0 && (p.charAt(i) - p.charAt(i - 1) == 1 || p.charAt(i - 1) - p.charAt(i) == 25)) {
+                maxLengthCur++;
+            }else {
+                maxLengthCur = 1;
+            }
+
+            int index = p.charAt(i) - 'a';
+            count[index] = Math.max(count[index], maxLengthCur);
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 26; i++) {
+            sum += count[i];
+        }
+        return sum;
+    }
+}
