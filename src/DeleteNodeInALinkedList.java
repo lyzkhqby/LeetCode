f3ffe22de278c171ee359c1a884b9b9c25b9f6e0
lyzkhqby <lyzkhqby@gmail.com>
2017-02-28 13:46:26 +0800

    Update DeleteNodeInALinkedList.java

--- src/DeleteNodeInALinkedList.java
+++ src/DeleteNodeInALinkedList.java
@@ -1,6 +1,11 @@
 /**
  * Created by rotoosoft-d04 on 2016/10/9.
  */
+
+/**Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked
+list should become 1 -> 2 -> 4 after calling your function.
+*/
 public class DeleteNodeInALinkedList {
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
