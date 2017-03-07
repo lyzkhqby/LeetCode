4934e755fa8eed71259d06c07478bc3981a2052b
lyzkhqby <lyzkhqby@gmail.com>
2017-03-07 10:49:38 +0800

    Update RemoveDuplicatesFromSortedList.java

--- src/RemoveDuplicatesFromSortedList.java
+++ src/RemoveDuplicatesFromSortedList.java
@@ -1,6 +1,12 @@
 /**
  * Created by rotoosoft-d04 on 2016/10/10.
  */
+/*
+    Given a sorted linked list, delete all duplicates such that each element appear only once.
+    For example,
+    Given 1->1->2, return 1->2.
+    Given 1->1->2->3->3, return 1->2->3.
+*/
 public class RemoveDuplicatesFromSortedList {
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null) {
