228980c196cc06f6ed75723d652ddf245fc06687
lyzkhqby <lyzkhqby@gmail.com>
2017-05-23 21:36:03 +0800

    Update BinaryTreeInorderTraversal.java

--- src/BinaryTreeInorderTraversal.java
+++ src/BinaryTreeInorderTraversal.java
@@ -4,6 +4,16 @@ import java.util.Stack;
 
 /**
  * Created by zk on 2016/5/1.
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+For example:
+Given binary tree [1,null,2,3],
+   1
+    \
+     2
+    /
+   3
+return [1,3,2].
  */
 public class BinaryTreeInorderTraversal {
     public List<Integer> inorderTraversal(TreeNode root) {
