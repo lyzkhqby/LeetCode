39a5e514ee869893c6ac5fc36bb8a811d7c97507
lyzkhqby <lyzkhqby@gmail.com>
2017-05-25 15:12:14 +0800

    Update SumOfLeftLeaves.java

--- src/SumOfLeftLeaves.java
+++ src/SumOfLeftLeaves.java
@@ -1,5 +1,16 @@
 /**
  * Created by zk on 2016/10/26.
+Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
  */
 public class SumOfLeftLeaves {
     public int sumOfLeftLeaves(TreeNode root) {
