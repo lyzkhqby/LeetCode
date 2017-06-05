8a3e48b4be346ab58bff2c2bb75d2bc480662c17
lyzkhqby <lyzkhqby@gmail.com>
2017-06-05 23:05:03 +0800

    Update RomanToInteger.java

--- src/RomanToInteger.java
+++ src/RomanToInteger.java
@@ -3,6 +3,9 @@ import java.util.Map;
 
 /**
  * Created by zk on 2016/6/20.
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
  */
 public class RomanToInteger {
     public int romanToInt(String s) {
