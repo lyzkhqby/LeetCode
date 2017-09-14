d15b2ee5fe60a13d856cab2576fbdc95e6fd7706
lyzkhqby <lyzkhqby@gmail.com>
2017-09-14 21:20:21 +0800

    Update KthSmallestElementInABST.java

--- src/KthSmallestElementInABST.java
+++ src/KthSmallestElementInABST.java
@@ -3,6 +3,13 @@ import java.util.List;
 
 /**
  * Created by zk on 2016/4/24.
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Follow up:
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
  */
 public class KthSmallestElementInABST {
     public int kthSmallest(TreeNode root, int k) {
