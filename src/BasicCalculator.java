e32705b534a0e541d796b4fcf8627c3f2fa9a72a
lyzkhqby <lyzkhqby@gmail.com>
2017-11-30 15:38:26 +0800

    Update BasicCalculator.java

--- src/BasicCalculator.java
+++ src/BasicCalculator.java
@@ -2,6 +2,16 @@ import java.util.Stack;
 
 /**
  * Created by zk on 2016/9/11.
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+
+You may assume that the given expression is always valid.
+
+Some examples:
+"1 + 1" = 2
+" 2-1 + 2 " = 3
+"(1+(4+5+2)-3)+(6+8)" = 23
  */
 public class BasicCalculator {
     public int calculate(String s) {
@@ -28,7 +38,7 @@ public class BasicCalculator {
                 result = 0;
                 sign = 1;
             }else if (c == ')') {
-                result = result * stack.pop() + stack.pop();
+                result = result * stack.pop() + stack.pop(); //乘的是sign
             }
         }
 
