5b2f6900327efcfb7eaad95598354d2aede3ec72
lyzkhqby <lyzkhqby@gmail.com>
2018-03-26 14:37:23 +0800

    Update Permutations.java

--- src/Permutations.java
+++ src/Permutations.java
@@ -3,6 +3,19 @@ import java.util.List;
 
 /**
  * Created by zk on 2016/10/26.
+ Given a collection of distinct numbers, return all possible permutations.
+
+For example,
+[1,2,3] have the following permutations:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
  */
 public class Permutations {
     public List<List<Integer>> permute(int[] nums) {
