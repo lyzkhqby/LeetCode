797cff6acb237d2edb7d5b94b500f44ff12a9099
rotoosoft-zk <lyzkhqby@sina.com>
2018-04-25 16:13:22 +0800

    new

---
+++ src/SearchA2DMatrix.java
@@ -0,0 +1,51 @@
+/**
+ * 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+ *
+ * 每行中的整数从左到右按升序排列。
+ * 每行的第一个整数大于前一行的最后一个整数。
+ * 示例 1:
+ *
+ * 输入:
+ * matrix = [
+ *   [1,   3,  5,  7],
+ *   [10, 11, 16, 20],
+ *   [23, 30, 34, 50]
+ * ]
+ * target = 3
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入:
+ * matrix = [
+ *   [1,   3,  5,  7],
+ *   [10, 11, 16, 20],
+ *   [23, 30, 34, 50]
+ * ]
+ * target = 13
+ * 输出: false
+ */
+public class SearchA2DMatrix {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) return false;
+        if (matrix[0] == null || matrix[0].length == 0) return false;
+
+        int row = matrix.length;
+        int column = matrix[0].length;
+
+        int start = 0, end = row * column - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int number = matrix[mid / column][mid % column];
+            if (number == target) {
+                return true;
+            } else if (number > target){
+                end = mid - 1;
+            }else {
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/Subsets.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ *
+ * 说明：解集不能包含重复的子集。
+ *
+ * 示例:
+ *
+ * 输入: nums = [1,2,3]
+ * 输出:
+ * [
+ *   [3],
+ *   [1],
+ *   [2],
+ *   [1,2,3],
+ *   [1,3],
+ *   [2,3],
+ *   [1,2],
+ *   []
+ * ]
+ */
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        if (nums == null && nums.length == 0) return results;
+        Arrays.sort(nums);
+        helper(new ArrayList<>(), nums, 0, results);
+        return results;
+    }
+
+    private void helper(ArrayList<Integer> subset, int[] nums, int startIndex, List<List<Integer>> results) {
+
+        results.add(new ArrayList<>(subset));
+
+        for (int i = startIndex; i < nums.length; i++) {
+            subset.add(nums[i]);
+            helper(subset, nums, i + 1, results);
+            subset.remove(subset.size() - 1);
+        }
+
+    }
+}
