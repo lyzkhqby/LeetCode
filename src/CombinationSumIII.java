a1b711f8522ce5823864d105dc54bb381da63c72
rotoosoft-zk <lyzkhqby@sina.com>
2018-05-11 18:46:34 +0800

    new

---
+++ src/CombinationSumIII.java
@@ -0,0 +1,40 @@
+/**
+ * 找出所有相加之和为 n 的 k 个数的组合。组合中只允许含有1 - 9的正整数，并且每种组合中不存在重复的数字。
+ *
+ * 说明：
+ *
+ * 所有数字都是正整数。
+ * 解集不能包含重复的组合。
+ * 示例 1:
+ *
+ * 输入: k = 3, n = 7
+ * 输出: [[1,2,4]]
+ * 示例 2:
+ *
+ * 输入: k = 3, n = 9
+ * 输出: [[1,2,6], [1,3,5], [2,3,4]]
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSumIII {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        combination(ans, new ArrayList<Integer>(), k, 1, n);
+        return ans;
+    }
+
+    private void combination(List<List<Integer>> ans, List<Integer> comb, int k,  int start, int n) {
+        if (comb.size() == k && n == 0) {
+            List<Integer> li = new ArrayList<Integer>(comb);
+            ans.add(li);
+            return;
+        }
+        for (int i = start; i <= 9; i++) {
+            comb.add(i);
+            combination(ans, comb, k, i+1, n-i);
+            comb.remove(comb.size() - 1);
+        }
+    }
+}
