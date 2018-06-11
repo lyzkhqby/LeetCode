01448ab26ec016404aa7228df7ef975eb3b9cc2b
lyzkhqby <lyzkhqby@sina.com>
2018-06-11 11:45:59 +0800

    easy

--- src/easy/MaximumSubarray.java
+++ src/easy/MaximumSubarray.java
@@ -1,8 +1,18 @@
-/*
+package easy;/*
 Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
 
 For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
 the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+示例:
+
+输入: [-2,1,-3,4,-1,2,1,-5,4],
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+进阶:
+
+如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
  */
 
 public class MaximumSubarray {
