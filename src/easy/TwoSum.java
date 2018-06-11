c463e66592eeb912d02a67d407be84a853defc64
lyzkhqby <lyzkhqby@sina.com>
2018-06-11 11:29:21 +0800

    easy

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -1,5 +1,5 @@
-/*
- * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+package easy;
+/** Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
  You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
@@ -8,6 +8,17 @@
 
  Because nums[0] + nums[1] = 2 + 7 = 9,
  return [0, 1].
+
+ 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+
+ 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+
+ 示例:
+
+ 给定 nums = [2, 7, 11, 15], target = 9
+
+ 因为 nums[0] + nums[1] = 2 + 7 = 9
+ 所以返回 [0, 1]
  */
 
 
