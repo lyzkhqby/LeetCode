74ea850e96c90408c1e38253cfa63bf403fc3cd6
张恺 <lyzkhqby@sina.com>
2020-09-07 08:52:56 +0800

    update

--- src/dfs/BinaryTreePaths.java
+++ src/dfs/BinaryTreePaths.java
@@ -1,20 +1,30 @@
+package dfs;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by rotoosoft-d04 on 2016/4/18.
-Given a binary tree, return all root-to-leaf paths.
-
-For example, given the following binary tree:
-
-   1
- /   \
-2     3
- \
-  5
-All root-to-leaf paths are:
-
-["1->2->5", "1->3"]
+ * 给定一个二叉树，返回所有从根节点到叶子节点的路径。
+ *
+ * 说明：叶子节点是指没有子节点的节点。
+ *
+ * 示例:
+ * 输入:
+ *
+ *    1
+ *  /   \
+ * 2     3
+ *  \
+ *   5
+ *
+ * 输出: ["1->2->5", "1->3"]
+ *
+ * 解释: 所有根节点到叶子节点的路径为: 1->2->5, 1->3
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-paths
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class BinaryTreePaths {
     public List<String> binaryTreePaths(TreeNode root) {
