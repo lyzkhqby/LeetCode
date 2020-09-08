9bc3912dbee304e8b2c87233fc7c228d8a2af0de
张恺 <lyzkhqby@sina.com>
2020-09-08 14:11:06 +0800

    update

---
+++ src/graph/MinimumHeightTrees.java
@@ -0,0 +1,87 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by zk on 2016/10/23.
+ * 对于一个具有树特征的无向图，我们可选择任何一个节点作为根。图因此可以成为树，在所有可能的树中，
+ * 具有最小高度的树被称为最小高度树。给出这样的一个图，写出一个函数找到所有的最小高度树并返回他们的根节点。
+ *
+ * 格式
+ *
+ * 该图包含n个节点，标记为0到n - 1。给定数字n和一个无向边edges列表（每一个边都是一对标签）。
+ *
+ * 你可以假设没有重复的边会出现在edges中。由于所有的边都是无向边， [0, 1]和[1, 0]是相同的，因此不会同时出现在edges里。
+ *
+ * 示例 1:
+ *
+ * 输入: n = 4, edges = [[1, 0], [1, 2], [1, 3]]
+ *
+ *         0
+ *         |
+ *         1
+ *        / \
+ *       2   3
+ *
+ * 输出: [1]
+ * 示例 2:
+ *
+ * 输入: n = 6, edges = [[0, 3], [1, 3], [2, 3], [4, 3], [5, 4]]
+ *
+ *      0  1  2
+ *       \ | /
+ *         3
+ *         |
+ *         4
+ *         |
+ *         5
+ *
+ * 输出: [3, 4]
+ * 说明:
+ *
+ * 根据树的定义，树是一个无向图，其中任何两个顶点只通过一条路径连接。 换句话说，一个任何没有简单环路的连通图都是一棵树。
+ * 树的高度是指根节点和叶子节点之间最长向下路径上边的数量。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-height-trees
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MinimumHeightTrees {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> leaf = new ArrayList<>();
+        if (n <= 1) {
+            leaf.add(0);
+            return leaf;
+        }
+
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) graph.put(i, new ArrayList<>());
+
+        int[] neighbors = new int[n];
+        for (int[] edge : edges) {
+            neighbors[edge[0]]++;
+            neighbors[edge[1]]++;
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (graph.get(i).size() == 1) leaf.add(i);
+        }
+
+
+        while (n > 2) {
+            List<Integer> newLeaf = new ArrayList<>();
+            for (int l : leaf) {
+                n--;
+                for (int nb : graph.get(l)) {
+                    if (--neighbors[nb] == 1) newLeaf.add(nb);
+                }
+            }
+            leaf = newLeaf;
+        }
+        return leaf;
+    }
+}
--- src/unionfind/EvaluateDivision.java
+++ src/unionfind/EvaluateDivision.java
@@ -1,9 +1,32 @@
+package unionfind;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
 /**
  * Created by zk on 2016/11/6.
+ * 给出方程式A / B = k, 其中A 和B 均为用字符串表示的变量，k 是一个浮点型数字。根据已知方程式求解问题，并返回计算结果。如果结果不存在，则返回-1.0。
+ *
+ * 示例 :
+ * 给定a / b = 2.0, b / c = 3.0
+ * 问题: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+ * 返回[6.0, 0.5, -1.0, 1.0, -1.0 ]
+ *
+ * 输入为: vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>>
+ *     queries(方程式，方程式结果，问题方程式)，其中equations.size() == values.size()，
+ * 即方程式的长度与方程式结果长度相等（程式与结果一一对应），并且结果值均为正数。以上为方程式的描述。返回vector<double>类型。
+ *
+ * 基于上述例子，输入如下：
+ *
+ * equations(方程式) = [ ["a", "b"], ["b", "c"] ],
+ * values(方程式结果) = [2.0, 3.0],
+ * queries(问题方程式) = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ].
+ * 输入总是有效的。你可以假设除法运算中不会出现除数为0的情况，且不存在任何矛盾的结果。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/evaluate-division
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class EvaluateDivision {
     public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
