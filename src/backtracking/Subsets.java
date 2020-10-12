5328654d45c42d78469291726b12e7acc395f0fd
张恺 <lyzkhqby@sina.com>
2020-10-12 20:55:49 +0800

    update

--- src/backtracking/Subsets.java
+++ src/backtracking/Subsets.java
@@ -28,20 +28,16 @@ public class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> results = new ArrayList<>();
         if (nums == null && nums.length == 0) return results;
-        Arrays.sort(nums);
         helper(new ArrayList<>(), nums, 0, results);
         return results;
     }
 
     private void helper(ArrayList<Integer> subset, int[] nums, int startIndex, List<List<Integer>> results) {
-
         results.add(new ArrayList<>(subset));
-
         for (int i = startIndex; i < nums.length; i++) {
             subset.add(nums[i]);
             helper(subset, nums, i + 1, results);
             subset.remove(subset.size() - 1);
         }
-
     }
 }
